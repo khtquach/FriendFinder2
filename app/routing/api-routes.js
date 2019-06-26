8fbb2e2d30f52aa50a1bbc69d4b40ed2b650688c
Kim Helen Quach <KimHelenQuach@campus-009-084.ucdavis.edu>
2019-06-25 19:03:50 -0700

    second to last commit, before finishing

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,5 +1,8 @@
+//list of "friends"
 var friendsList = require("../data/friends")
 
+
+//ya girl's routes for the friends list
 module.exports = function (app) {
     app.get("/api/friends", function (req, res){
         res.json(friendsList)
